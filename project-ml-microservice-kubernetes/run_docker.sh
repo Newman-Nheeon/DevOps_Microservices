1d834f3de93ba19047b40c32a9362da337d16f83
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2022-09-06 11:49:05 +0000

    build docker image

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=nheeon/devopsproject04 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 nheeon/devopsproject04
